c27a4b2e23fcef4b37cd90c6f0371a6a245e5124
khushi.frontend <khushi.jain@staqu.com>
2024-06-05 15:20:27 +0530

    first commit

---
+++ gemini-bot.py
@@ -0,0 +1,61 @@
+import streamlit as st 
+import os 
+import google.generativeai as genai 
+
+
+st.title("Chatster")
+
+os.environ['GOOGLE_API_KEY'] = "AIzaSyBoAvbBC0lynhM4jQQ6y4Pd19BYhOHktwE"
+genai.configure(api_key = os.environ['GOOGLE_API_KEY'])
+
+##Select the model
+model = genai.GenerativeModel('gemini-pro')
+
+## Initialize Chat History
+if "messages" not in st.session_state:
+    st.session_state.messages = [
+        {
+            "role":"assistant",
+            "content":"Ask me Anythinggggg...."
+        }
+    ]
+
+##Display chant messages on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+##Process and store query and response
+def llm_function(query):
+    response = model.generate_content(query)
+
+    ##Displaying the assistant message
+    with st.chat_message("assistant"):
+        st.markdown(response.text)
+
+    ##Storing the user message
+    st.session_state.messages.append(
+        {
+            "role": "user",
+            "content":query
+        }
+    )
+
+    ##Storing the assistant message
+    st.session_state.messages.append(
+        {
+            "role":"assistant",
+            "content": response.text
+        }
+    )
+
+ ##Accept user inpit
+query = st.chat_input("What's Uppp????")
+
+##Calling the function when input is provided
+if query:
+    #Displaying the user message
+    with st.chat_message("user"):
+        st.markdown(query)
+
+    llm_function(query)
